8e6aeedf19dee92780d17bee2b937c8b3c620e56
NF38 <93057555+NF38@users.noreply.github.com>
2022-08-02 08:59:43 +0300

    fixing styling clash

--- src/components/directory-item/directory-item.component.js
+++ src/components/directory-item/directory-item.component.js
@@ -1,15 +1,15 @@
-import "./category.styles.scss";
+import "./directory.styles.scss";
 
-const CategoryItem = ({ category }) => {
+const DirectoryItem = ({ category }) => {
   const { imageUrl, title } = category;
 
   return (
-    <div className="category-container">
+    <div className="directory-item-container">
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
       />
-      <div className="category-body-container">
+      <div className="body">
         <h2>{title.toUpperCase()}</h2>
         <p>SHOP NOW</p>
       </div>
@@ -17,4 +17,4 @@ const CategoryItem = ({ category }) => {
   );
 };
 
-export default CategoryItem;
+export default DirectoryItem;
