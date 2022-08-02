d7c4a3a9bf5ebed9fdea81229aa4659dcfec92c7
NF38 <93057555+NF38@users.noreply.github.com>
2022-08-02 09:16:44 +0300

    added nested route functionality

--- src/components/category-preview/category-preview.component.js
+++ src/components/category-preview/category-preview.component.js
@@ -1,12 +1,16 @@
-import "./category-preview.styles.scss";
+import { Link } from "react-router-dom";
 
 import ProductCard from "../product-card/product-card.component";
+
+import "./category-preview.styles.scss";
 
 const CategoryPreview = ({ title, products }) => {
   return (
     <div className="category-preview-container">
       <h2>
-        <span className="title">{title.toUpperCase()}</span>
+        <Link className="title" to={title}>
+          {title.toUpperCase()}
+        </Link>
       </h2>
       <div className="preview">
         {products
--- src/routes/category/category.component.js
+++ src/routes/category/category.component.js
@@ -17,12 +17,15 @@ const Category = () => {
   }, [category, categoriesMap]);
 
   return (
-    <div className="category-container">
-      {products &&
-        products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-    </div>
+    <>
+      <h2 className="category-title">{category.toUpperCase()}</h2>
+      <div className="category-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </>
   );
 };
 
