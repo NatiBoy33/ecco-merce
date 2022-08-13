81436a5ec288a4e1d3e1673c681eceb1724137bf
NF38 <93057555+NF38@users.noreply.github.com>
2022-08-13 11:28:52 +0300

    remove .env

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -22,12 +22,12 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAN5peKXPsnqEvvpnjAaZjkUvvwiUDZtTM",
+  authDomain: "ecco-merce.firebaseapp.com",
+  projectId: "ecco-merce",
+  storageBucket: "ecco-merce.appspot.com",
+  messagingSenderId: "961077903607",
+  appId: "1:961077903607:web:f951db1b6bdd4dbb9f93f2",
 };
 
 // Initialize Firebase
